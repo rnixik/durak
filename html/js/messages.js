7dfa256db845a083cad49ea398d402e75dc0d46b
rNix <rnix@yandex.ru>
2018-11-25 19:19:49 +0700

    Add more translations to lobby and game

--- html/js/messages.js
+++ html/js/messages.js
@@ -6,15 +6,37 @@ window.i18nMessages = {
     },
     ru: {
         lobby: {
+            clients: 'Клиенты',
             your_nickname: 'Ваш псевдоним',
             your_id: 'Ваш ID',
-            clients: 'Клиенты',
             rooms: 'Комнаты',
             room: 'Комната',
             here: 'Вы здесь',
+            your_are_owner: 'вы создатель',
             game_status: {
                 preparing: 'Подготовка',
             },
-        }
+            mark_as_player: 'Отменить как игрока',
+            mark_as_spectator: 'Отменить как зрителя',
+            create_new_room: 'Создать новую комнату',
+            want_to_play: 'Хочу играть',
+            want_to_spectate: 'Хочу смотреть',
+            start_game: 'Начать игру',
+            game: 'Игра',
+        },
+        game: {
+            pick_up: 'Взять',
+            complete: 'Готово',
+            is_looser: 'проиграл',
+            draw: 'ничья',
+            you_are_attacker: 'Вы ходите',
+            is_attacker: 'ходит',
+            reason: 'Причина',
+            you_are_defender: 'Вы защищаетесь',
+            attacker: 'Атакующий',
+            defender: 'Обороняющийся',
+            game_is_over: 'Игра окончена',
+        },
+        error: 'Ошибка'
     },
 };
