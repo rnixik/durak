40efb5bd442988afc86d5681adbe66de39e65875
Roman Nix <rnix@yandex.ru>
2020-06-25 14:22:30 +0700

    Improve AI: do not attack with powerful cards in the beginning

--- bot.go
+++ bot.go
@@ -5,16 +5,19 @@ import (
 	"log"
 )
 
+const additionalAttackStopIndex = float64(0.25)
+
 // Bot represents a AI player
 type Bot struct {
-	botClient       *BotClient
-	gameStateInfo   *GameStateInfo
-	players         []*Player
-	yourPlayerIndex int
-	gameWasStarted  bool
-	gameIsOver      bool
-	myUnbeatenCards map[Card]bool
-	iAmPickingUp    bool
+	botClient         *BotClient
+	gameStateInfo     *GameStateInfo
+	players           []*Player
+	yourPlayerIndex   int
+	gameWasStarted    bool
+	gameIsOver        bool
+	myUnbeatenCards   map[Card]bool
+	iAmPickingUp      bool
+	initialPlayersNum int
 }
 
 func newBot(botClient *BotClient) *Bot {
@@ -42,8 +45,6 @@ func (b *Bot) dispatchEvent(message []byte) {
 		log.Printf("BOT: cannot decode general event: %s", err)
 		return
 	}
-
-	log.Printf("BOT: event %+v", event)
 
 	eventDataJson, err := json.Marshal(event.Data)
 	if err != nil {
@@ -146,6 +147,8 @@ func (b *Bot) onGameFirstAttackerEvent(event GameFirstAttackerEvent) {
 func (b *Bot) onGameStartedEvent(event GameStartedEvent) {
 	b.gameStateInfo = event.GameStateInfo
 	b.gameWasStarted = true
+	b.gameIsOver = false
+	b.initialPlayersNum = len(b.players)
 }
 
 func (b *Bot) onGameAttackEvent(event GameAttackEvent) {
@@ -226,14 +229,23 @@ func (b *Bot) getAvailableCardsForAttack() (cards []*Card) {
 	return cards
 }
 
-func (b *Bot) attack() {
+func (b *Bot) attack() bool {
 	b.printHand()
 	availableCards := b.getAvailableCardsForAttack()
 	minimalValueCard := b.findLowestCard(availableCards)
+
+	// Should bot add card to strong cards on table?
+	battlegroundPickUpValue := b.getTablePickUpValue(minimalValueCard)
+	if battlegroundPickUpValue > additionalAttackStopIndex {
+		log.Printf("STOPPED ATTACK BY PICKUP VALUE = %f (with card = %s)", battlegroundPickUpValue, minimalValueCard.Value)
+		return false
+	}
+
 	attackActionData := AttackActionData{Card: minimalValueCard}
 	b.botClient.sendGameAction(PlayerActionNameAttack, attackActionData)
-	log.Printf("BOT: attack with %+v", minimalValueCard)
 	b.myUnbeatenCards[*minimalValueCard] = true
+
+	return true
 }
 
 func (b *Bot) canDefend() bool {
@@ -306,9 +318,21 @@ func (b *Bot) makeDecision() {
 	}
 
 	if b.canAttack() {
-		log.Println("BOT: Can attack")
-		b.attack()
-		return
+		if len(b.gameStateInfo.Battleground) == 0 {
+			if b.attack() {
+				return
+			}
+
+		}
+		// Should bot add card to strong cards on table?
+		battlegroundPickUpValue := b.getTablePickUpValue(nil)
+		if battlegroundPickUpValue < additionalAttackStopIndex {
+			if b.attack() {
+				return
+			}
+		} else {
+			log.Printf("STOPPED ATTACK BY PICKUP VALUE = %f", battlegroundPickUpValue)
+		}
 	}
 
 	log.Println("BOT: Can't attack")
@@ -362,6 +386,134 @@ func (b *Bot) findLowestCard(cards []*Card) *Card {
 	}
 
 	return minimalValueCard
+}
+
+func (b *Bot) getTablePickUpValue(possibleCard *Card) float64 {
+	if len(b.gameStateInfo.Battleground) == 0 {
+		return float64(0)
+	}
+
+	// How many cards left in deck: 0..1: 0 - empty deck; 1 - full deck.
+	deckRemainsIndex := float64(0)
+	if b.initialPlayersNum < 6 {
+		deckRemainsIndex = float64(b.gameStateInfo.DeckSize) / float64(36-b.initialPlayersNum*6)
+	}
+
+	// Each card has attack rate from 0 ("6") to 9 ("A")
+	// Each trump card has attack rate from 10 (trump "6") to 18 (trump "A")
+	// maxAttackRate - cards with highest value: trump "A", "K", "Q", ...
+	// We need this value to get attack rate of battleground in range of 0..1
+	maxAttackRate := 0
+	maxAttackRatePerCurrentCard := 18 // Trump "A" has maximum attack rate
+
+	totalCardsOnTable := len(b.gameStateInfo.Battleground) + len(b.gameStateInfo.DefendingCards)
+	if possibleCard != nil {
+		totalCardsOnTable += 1
+	}
+	for i := 0; i < totalCardsOnTable; i++ {
+		maxAttackRate += maxAttackRatePerCurrentCard
+		maxAttackRatePerCurrentCard -= 1
+	}
+
+	battlegroundAttackRate := 0
+	for _, card := range b.gameStateInfo.Battleground {
+		if card.Suit == b.gameStateInfo.TrumpCard.Suit {
+			battlegroundAttackRate += card.getValueIndex() + 9
+		} else {
+			battlegroundAttackRate += card.getValueIndex()
+		}
+	}
+	for _, card := range b.gameStateInfo.DefendingCards {
+		if card.Suit == b.gameStateInfo.TrumpCard.Suit {
+			battlegroundAttackRate += card.getValueIndex() + 9
+		} else {
+			battlegroundAttackRate += card.getValueIndex()
+		}
+	}
+
+	if possibleCard != nil {
+		if possibleCard.Suit == b.gameStateInfo.TrumpCard.Suit {
+			battlegroundAttackRate += possibleCard.getValueIndex() + 9
+		} else {
+			battlegroundAttackRate += possibleCard.getValueIndex()
+		}
+	}
+
+	battlegroundAttackRateIndex := float64(0)
+	if maxAttackRate > 0 {
+		battlegroundAttackRateIndex = float64(battlegroundAttackRate) / float64(maxAttackRate)
+	}
+
+	// How many pairs on table
+	pairNumberCards := b.getCardsNumberOnTable()
+	if possibleCard != nil {
+		if _, ok := pairNumberCards[possibleCard.Value]; ok {
+			pairNumberCards[possibleCard.Value] += 1
+		} else {
+			pairNumberCards[possibleCard.Value] = 1
+		}
+	}
+
+	pair2Index := getCardsPairsIndex(pairNumberCards, 2)
+	pair3Index := getCardsPairsIndex(pairNumberCards, 3)
+	pair4Index := getCardsPairsIndex(pairNumberCards, 4)
+
+	cardsPowerIndex := (9*battlegroundAttackRateIndex + pair2Index/2 + pair3Index + 2*pair4Index) / float64(13)
+
+	value := deckRemainsIndex * cardsPowerIndex
+
+	log.Printf(
+		"pickup value = %f, deck index = %f, attack rate index = %f, pair2 = %f, pair3 = %f, pair4 = %f, cards power = %f",
+		value,
+		deckRemainsIndex,
+		battlegroundAttackRateIndex,
+		pair2Index,
+		pair3Index,
+		pair4Index,
+		cardsPowerIndex,
+	)
+
+	return value
+}
+
+func (b *Bot) getCardsNumberOnTable() map[string]int {
+	cards := make(map[string]int, 0)
+	for _, card := range b.gameStateInfo.Battleground {
+		if _, ok := cards[card.Value]; ok {
+			cards[card.Value] += 1
+		} else {
+			cards[card.Value] = 1
+		}
+	}
+	for _, card := range b.gameStateInfo.DefendingCards {
+		if _, ok := cards[card.Value]; ok {
+			cards[card.Value] += 1
+		} else {
+			cards[card.Value] = 1
+		}
+	}
+
+	return cards
+}
+
+func getCardsPairsIndex(cards map[string]int, pairSize int) float64 {
+	totalCards := 0
+	pairsNumber := 0
+	for _, number := range cards {
+		totalCards += number
+		if number == pairSize {
+			pairsNumber += 1
+		}
+	}
+
+	maxPairsNumber := totalCards / pairSize
+
+	index := float64(0)
+	if maxPairsNumber > 0 {
+		index = float64(pairsNumber) / float64(maxPairsNumber)
+	}
+
+	return index
 }
 
 func (b *Bot) printHand() {
