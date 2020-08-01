06c830adbb07afd3829ecdb9a2649a13720e1521
Roman Nix <rnix@yandex.ru>
2020-08-01 20:02:02 +0700

    Fix CA in docker and project name

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 
 cd "${DIR}/.."
 echo $(( $(cat version) + 1 )) > version
-git pull && docker-compose -f deployments/docker-compose.prod.yml up -d --build
+git pull && docker-compose -p durak -f deployments/docker-compose.prod.yml up -d --build
 cd -
